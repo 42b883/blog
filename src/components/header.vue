72c6c05c8e1b030e82a5eb8e9ee59d2957ee6c17
42b883 <42b883@gmail.com>
2018-10-10 19:52:27 +0600

    routing and route parameters

---
+++ src/components/header.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav>
+        <ul>
+            <li><router-link to="/" exact>Blog</router-link></li>
+            <li><router-link to="/add" exact>Add a New Blog</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style-type: none;
+        text-align: center;
+        margin: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 2px 10px 2px 10px;
+        border: 2px solid transparent;
+    }
+    li:hover {
+        background: rgb(58, 180, 129);
+        border:2px solid #50b482;
+        background: rgba(0,0,0,0);
+    }
+    a {
+        color: #ffffff;
+        text-decoration: none;
+        padding: 6px 8px;
+        border-radius: 10px;
+    }
+    nav {
+        background: #002;
+        padding: 14px 0;
+        margin-bottom: 40px;
+        height: 24px;
+    }
+    .router-link-active {
+
+    }
+</style>
+
